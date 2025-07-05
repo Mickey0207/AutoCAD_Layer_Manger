bd02e449d07eb678ce12d647ab54c113a3c304b1
Mickey0207 <157815247+Mickey0207@users.noreply.github.com>
2025-07-05 21:06:05 +0800

    UI 優化

--- UI/LayerManagerForm.Designer.cs
+++ UI/LayerManagerForm.Designer.cs
@@ -28,10 +28,181 @@ namespace AutoCAD_Layer_Manger.UI
         /// </summary>
         private void InitializeComponent()
         {
-            // This method is implemented in the main form file
-            // for better integration with the Designer
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.layerGroupBox = new System.Windows.Forms.GroupBox();
+            this.previewButton = new System.Windows.Forms.Button();
+            this.layerComboBox = new System.Windows.Forms.ComboBox();
+            this.settingsGroupBox = new System.Windows.Forms.GroupBox();
+            this.handleLockedObjectsCheckBox = new System.Windows.Forms.CheckBox();
+            this.createLayerCheckBox = new System.Windows.Forms.CheckBox();
+            this.unlockTargetLayerCheckBox = new System.Windows.Forms.CheckBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.layerGroupBox.SuspendLayout();
+            this.settingsGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.infoLabel.ForeColor = System.Drawing.Color.DarkBlue;
+            this.infoLabel.Location = new System.Drawing.Point(12, 15);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(102, 20);
+            this.infoLabel.TabIndex = 0;
+            this.infoLabel.Text = "已選取物件";
+            // 
+            // layerGroupBox
+            // 
+            this.layerGroupBox.Controls.Add(this.previewButton);
+            this.layerGroupBox.Controls.Add(this.layerComboBox);
+            this.layerGroupBox.Location = new System.Drawing.Point(12, 50);
+            this.layerGroupBox.Name = "layerGroupBox";
+            this.layerGroupBox.Size = new System.Drawing.Size(480, 80);
+            this.layerGroupBox.TabIndex = 1;
+            this.layerGroupBox.TabStop = false;
+            this.layerGroupBox.Text = "目標圖層";
+            // 
+            // previewButton
+            // 
+            this.previewButton.Location = new System.Drawing.Point(380, 29);
+            this.previewButton.Name = "previewButton";
+            this.previewButton.Size = new System.Drawing.Size(75, 25);
+            this.previewButton.TabIndex = 1;
+            this.previewButton.Text = "預覽";
+            this.previewButton.UseVisualStyleBackColor = true;
+            this.previewButton.Click += new System.EventHandler(this.previewButton_Click);
+            // 
+            // layerComboBox
+            // 
+            this.layerComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.layerComboBox.FormattingEnabled = true;
+            this.layerComboBox.Location = new System.Drawing.Point(15, 30);
+            this.layerComboBox.Name = "layerComboBox";
+            this.layerComboBox.Size = new System.Drawing.Size(350, 20);
+            this.layerComboBox.TabIndex = 0;
+            // 
+            // settingsGroupBox
+            // 
+            this.settingsGroupBox.Controls.Add(this.handleLockedObjectsCheckBox);
+            this.settingsGroupBox.Controls.Add(this.createLayerCheckBox);
+            this.settingsGroupBox.Controls.Add(this.unlockTargetLayerCheckBox);
+            this.settingsGroupBox.Location = new System.Drawing.Point(12, 150);
+            this.settingsGroupBox.Name = "settingsGroupBox";
+            this.settingsGroupBox.Size = new System.Drawing.Size(480, 150);
+            this.settingsGroupBox.TabIndex = 2;
+            this.settingsGroupBox.TabStop = false;
+            this.settingsGroupBox.Text = "轉換設定";
+            // 
+            // handleLockedObjectsCheckBox
+            // 
+            this.handleLockedObjectsCheckBox.AutoSize = true;
+            this.handleLockedObjectsCheckBox.Checked = true;
+            this.handleLockedObjectsCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.handleLockedObjectsCheckBox.Location = new System.Drawing.Point(15, 90);
+            this.handleLockedObjectsCheckBox.Name = "handleLockedObjectsCheckBox";
+            this.handleLockedObjectsCheckBox.Size = new System.Drawing.Size(147, 16);
+            this.handleLockedObjectsCheckBox.TabIndex = 2;
+            this.handleLockedObjectsCheckBox.Text = "處理鎖定圖層上的物件";
+            this.handleLockedObjectsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // createLayerCheckBox
+            // 
+            this.createLayerCheckBox.AutoSize = true;
+            this.createLayerCheckBox.Checked = true;
+            this.createLayerCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.createLayerCheckBox.Location = new System.Drawing.Point(15, 60);
+            this.createLayerCheckBox.Name = "createLayerCheckBox";
+            this.createLayerCheckBox.Size = new System.Drawing.Size(159, 16);
+            this.createLayerCheckBox.TabIndex = 1;
+            this.createLayerCheckBox.Text = "自動創建不存在的圖層";
+            this.createLayerCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // unlockTargetLayerCheckBox
+            // 
+            this.unlockTargetLayerCheckBox.AutoSize = true;
+            this.unlockTargetLayerCheckBox.Checked = true;
+            this.unlockTargetLayerCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.unlockTargetLayerCheckBox.Location = new System.Drawing.Point(15, 30);
+            this.unlockTargetLayerCheckBox.Name = "unlockTargetLayerCheckBox";
+            this.unlockTargetLayerCheckBox.Size = new System.Drawing.Size(135, 16);
+            this.unlockTargetLayerCheckBox.TabIndex = 0;
+            this.unlockTargetLayerCheckBox.Text = "自動解鎖目標圖層";
+            this.unlockTargetLayerCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.ForeColor = System.Drawing.Color.Gray;
+            this.statusLabel.Location = new System.Drawing.Point(12, 320);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(53, 12);
+            this.statusLabel.TabIndex = 3;
+            this.statusLabel.Text = "準備就緒";
+            // 
+            // convertButton
+            // 
+            this.convertButton.BackColor = System.Drawing.Color.LightGreen;
+            this.convertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.convertButton.Location = new System.Drawing.Point(330, 360);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(80, 35);
+            this.convertButton.TabIndex = 4;
+            this.convertButton.Text = "開始轉換";
+            this.convertButton.UseVisualStyleBackColor = false;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(420, 360);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 35);
+            this.cancelButton.TabIndex = 5;
+            this.cancelButton.Text = "取消";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // LayerManagerForm
+            // 
+            this.AcceptButton = this.convertButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(520, 420);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.settingsGroupBox);
+            this.Controls.Add(this.layerGroupBox);
+            this.Controls.Add(this.infoLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LayerManagerForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AutoCAD 圖層管理器";
+            this.layerGroupBox.ResumeLayout(false);
+            this.settingsGroupBox.ResumeLayout(false);
+            this.settingsGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.GroupBox layerGroupBox;
+        private System.Windows.Forms.Button previewButton;
+        private System.Windows.Forms.ComboBox layerComboBox;
+        private System.Windows.Forms.GroupBox settingsGroupBox;
+        private System.Windows.Forms.CheckBox handleLockedObjectsCheckBox;
+        private System.Windows.Forms.CheckBox createLayerCheckBox;
+        private System.Windows.Forms.CheckBox unlockTargetLayerCheckBox;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Button cancelButton;
     }
 }
--- UI/LayerManagerForm.cs
+++ UI/LayerManagerForm.cs
@@ -18,187 +18,26 @@ namespace AutoCAD_Layer_Manger.UI
         private readonly ObjectId[] _entityIds;
         public ConversionResult? Result { get; private set; }
 
-        // UI 控制項
-        private ComboBox layerComboBox;
-        private CheckBox unlockTargetLayerCheckBox;
-        private CheckBox createLayerCheckBox;
-        private CheckBox handleLockedObjectsCheckBox;
-        private Button convertButton;
-        private Button cancelButton;
-        private Button previewButton;
-        private Label statusLabel;
-        private Label infoLabel;
-        private GroupBox layerGroupBox;
-        private GroupBox settingsGroupBox;
-
         public LayerManagerForm()
         {
             _entityIds = Array.Empty<ObjectId>();
-            InitializeComponents();
+            InitializeComponent();
         }
 
         public LayerManagerForm(ObjectId[] entityIds)
         {
             _entityIds = entityIds;
-            InitializeComponents();
-            LoadLayers();
+            InitializeComponent();
+            InitializeFormData();
         }
 
-        private void InitializeComponents()
+        private void InitializeFormData()
         {
-            this.layerComboBox = new ComboBox();
-            this.unlockTargetLayerCheckBox = new CheckBox();
-            this.createLayerCheckBox = new CheckBox();
-            this.handleLockedObjectsCheckBox = new CheckBox();
-            this.convertButton = new Button();
-            this.cancelButton = new Button();
-            this.previewButton = new Button();
-            this.statusLabel = new Label();
-            this.infoLabel = new Label();
-            this.layerGroupBox = new GroupBox();
-            this.settingsGroupBox = new GroupBox();
-            this.layerGroupBox.SuspendLayout();
-            this.settingsGroupBox.SuspendLayout();
-            this.SuspendLayout();
+            // 設定物件數量資訊
+            this.infoLabel.Text = $"已選取 {_entityIds.Length} 個物件";
             
-            // LayerManagerForm
-            this.AutoScaleDimensions = new SizeF(7F, 15F);
-            this.AutoScaleMode = AutoScaleMode.Font;
-            this.ClientSize = new Size(520, 450);
-            this.FormBorderStyle = FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "LayerManagerForm";
-            this.StartPosition = FormStartPosition.CenterScreen;
-            this.Text = "AutoCAD 圖層管理器";
-            
-            // infoLabel
-            this.infoLabel.AutoSize = true;
-            this.infoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            this.infoLabel.ForeColor = Color.DarkBlue;
-            this.infoLabel.Location = new Point(12, 15);
-            this.infoLabel.Name = "infoLabel";
-            this.infoLabel.Size = new Size(200, 20);
-            this.infoLabel.TabIndex = 0;
-            this.infoLabel.Text = "已選取物件";
-            
-            // layerGroupBox
-            this.layerGroupBox.Controls.Add(this.layerComboBox);
-            this.layerGroupBox.Controls.Add(this.previewButton);
-            this.layerGroupBox.Location = new Point(12, 50);
-            this.layerGroupBox.Name = "layerGroupBox";
-            this.layerGroupBox.Size = new Size(480, 80);
-            this.layerGroupBox.TabIndex = 1;
-            this.layerGroupBox.TabStop = false;
-            this.layerGroupBox.Text = "目標圖層";
-            
-            // layerComboBox
-            this.layerComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
-            this.layerComboBox.FormattingEnabled = true;
-            this.layerComboBox.Location = new Point(15, 30);
-            this.layerComboBox.Name = "layerComboBox";
-            this.layerComboBox.Size = new Size(350, 23);
-            this.layerComboBox.TabIndex = 0;
-            
-            // previewButton
-            this.previewButton.Location = new Point(380, 29);
-            this.previewButton.Name = "previewButton";
-            this.previewButton.Size = new Size(75, 25);
-            this.previewButton.TabIndex = 1;
-            this.previewButton.Text = "預覽";
-            this.previewButton.UseVisualStyleBackColor = true;
-            this.previewButton.Click += this.PreviewButton_Click;
-            
-            // settingsGroupBox
-            this.settingsGroupBox.Controls.Add(this.unlockTargetLayerCheckBox);
-            this.settingsGroupBox.Controls.Add(this.createLayerCheckBox);
-            this.settingsGroupBox.Controls.Add(this.handleLockedObjectsCheckBox);
-            this.settingsGroupBox.Location = new Point(12, 150);
-            this.settingsGroupBox.Name = "settingsGroupBox";
-            this.settingsGroupBox.Size = new Size(480, 150);
-            this.settingsGroupBox.TabIndex = 2;
-            this.settingsGroupBox.TabStop = false;
-            this.settingsGroupBox.Text = "轉換設定";
-            
-            // unlockTargetLayerCheckBox
-            this.unlockTargetLayerCheckBox.AutoSize = true;
-            this.unlockTargetLayerCheckBox.Checked = true;
-            this.unlockTargetLayerCheckBox.CheckState = CheckState.Checked;
-            this.unlockTargetLayerCheckBox.Location = new Point(15, 30);
-            this.unlockTargetLayerCheckBox.Name = "unlockTargetLayerCheckBox";
-            this.unlockTargetLayerCheckBox.Size = new Size(135, 19);
-            this.unlockTargetLayerCheckBox.TabIndex = 0;
-            this.unlockTargetLayerCheckBox.Text = "自動解鎖目標圖層";
-            this.unlockTargetLayerCheckBox.UseVisualStyleBackColor = true;
-            
-            // createLayerCheckBox
-            this.createLayerCheckBox.AutoSize = true;
-            this.createLayerCheckBox.Checked = true;
-            this.createLayerCheckBox.CheckState = CheckState.Checked;
-            this.createLayerCheckBox.Location = new Point(15, 60);
-            this.createLayerCheckBox.Name = "createLayerCheckBox";
-            this.createLayerCheckBox.Size = new Size(159, 19);
-            this.createLayerCheckBox.TabIndex = 1;
-            this.createLayerCheckBox.Text = "自動創建不存在的圖層";
-            this.createLayerCheckBox.UseVisualStyleBackColor = true;
-            
-            // handleLockedObjectsCheckBox
-            this.handleLockedObjectsCheckBox.AutoSize = true;
-            this.handleLockedObjectsCheckBox.Checked = true;
-            this.handleLockedObjectsCheckBox.CheckState = CheckState.Checked;
-            this.handleLockedObjectsCheckBox.Location = new Point(15, 90);
-            this.handleLockedObjectsCheckBox.Name = "handleLockedObjectsCheckBox";
-            this.handleLockedObjectsCheckBox.Size = new Size(147, 19);
-            this.handleLockedObjectsCheckBox.TabIndex = 2;
-            this.handleLockedObjectsCheckBox.Text = "處理鎖定圖層上的物件";
-            this.handleLockedObjectsCheckBox.UseVisualStyleBackColor = true;
-            
-            // statusLabel
-            this.statusLabel.AutoSize = true;
-            this.statusLabel.ForeColor = Color.Gray;
-            this.statusLabel.Location = new Point(12, 320);
-            this.statusLabel.Name = "statusLabel";
-            this.statusLabel.Size = new Size(55, 15);
-            this.statusLabel.TabIndex = 3;
-            this.statusLabel.Text = "準備就緒";
-            
-            // convertButton
-            this.convertButton.BackColor = Color.LightGreen;
-            this.convertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            this.convertButton.Location = new Point(330, 360);
-            this.convertButton.Name = "convertButton";
-            this.convertButton.Size = new Size(80, 35);
-            this.convertButton.TabIndex = 4;
-            this.convertButton.Text = "開始轉換";
-            this.convertButton.UseVisualStyleBackColor = false;
-            this.convertButton.Click += this.ConvertButton_Click;
-            
-            // cancelButton
-            this.cancelButton.DialogResult = DialogResult.Cancel;
-            this.cancelButton.Location = new Point(420, 360);
-            this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new Size(75, 35);
-            this.cancelButton.TabIndex = 5;
-            this.cancelButton.Text = "取消";
-            this.cancelButton.UseVisualStyleBackColor = true;
-            
-            // 加入控制項到表單
-            this.Controls.Add(this.infoLabel);
-            this.Controls.Add(this.layerGroupBox);
-            this.Controls.Add(this.settingsGroupBox);
-            this.Controls.Add(this.statusLabel);
-            this.Controls.Add(this.convertButton);
-            this.Controls.Add(this.cancelButton);
-            
-            this.layerGroupBox.ResumeLayout(false);
-            this.settingsGroupBox.ResumeLayout(false);
-            this.settingsGroupBox.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-            
-            // 設定按鈕
-            this.AcceptButton = this.convertButton;
-            this.CancelButton = this.cancelButton;
+            // 載入圖層數據
+            LoadLayers();
         }
 
         private void LoadLayers()
@@ -206,7 +45,6 @@ namespace AutoCAD_Layer_Manger.UI
             try
             {
                 this.statusLabel.Text = "正在載入圖層...";
-                this.infoLabel.Text = $"已選取 {_entityIds.Length} 個物件";
                 
                 var doc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
                 if (doc?.Database == null) return;
@@ -253,7 +91,7 @@ namespace AutoCAD_Layer_Manger.UI
             }
         }
 
-        private void PreviewButton_Click(object sender, EventArgs e)
+        private void previewButton_Click(object sender, EventArgs e)
         {
             if (this.layerComboBox.SelectedItem is string selectedItem)
             {
@@ -270,7 +108,7 @@ namespace AutoCAD_Layer_Manger.UI
             }
         }
 
-        private void ConvertButton_Click(object sender, EventArgs e)
+        private void convertButton_Click(object sender, EventArgs e)
         {
             if (this.layerComboBox.SelectedItem is string selectedItem)
             {
